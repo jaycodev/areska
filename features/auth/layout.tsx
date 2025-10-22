3447514a4cf00323ee33ba948c88d965b7ea5586
jaycodev <jason.vilac@gmail.com>
2025-10-22 18:01:04 -0500

    fix(auth): restructure AuthLayout for improved button placement and layout consistency

--- features/auth/layout.tsx
+++ features/auth/layout.tsx
@@ -1,4 +1,8 @@
+import { ArrowLeft } from 'lucide-react'
+import Link from 'next/link'
+
 import { Logo } from '@/components/shared/logo'
+import { Button } from '@/components/ui/button'
 
 interface Props {
   children?: React.ReactNode
@@ -6,13 +10,21 @@ interface Props {
 
 export function AuthLayout({ children }: Props) {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <span className="flex items-center gap-2 self-center font-medium">
-          <Logo className="size-6" />
-          Areska
-        </span>
-        {children}
+    <div className="relative min-h-svh">
+      <Button variant="ghost" asChild className="absolute top-4 left-4">
+        <Link href="/">
+          <ArrowLeft />
+          Ir al inicio
+        </Link>
+      </Button>
+      <div className="flex items-center justify-center min-h-svh p-6 md:p-10">
+        <div className="w-full max-w-sm flex flex-col gap-6">
+          <span className="flex items-center gap-2 self-center font-medium">
+            <Logo className="size-6" />
+            Areska
+          </span>
+          {children}
+        </div>
       </div>
     </div>
   )
